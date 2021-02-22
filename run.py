40b176b18ebe6b54f92a9b0c243579651857e430
Gen Takagi <hamham37337@gmail.com>
2021-02-22 22:42:25 +0900

    new

--- run.py
+++ run.py
@@ -3,5 +3,5 @@ import src.train as train
 if __name__ == '__main__':
     model = train.Model(practie_name="vgg16-80-128-100", operation="ubuntu", image_size=80)
     model.images_create()
-    model.train(batch_size=11128)
+    model.train(batch_size=128)
     model.test()
