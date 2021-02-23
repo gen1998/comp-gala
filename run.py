527aa1e75fe2b71356880d78bb3b9da4add61306
Gen Takagi <hamham37337@gmail.com>
2021-02-23 21:25:46 +0900

    newmodel

--- run.py
+++ run.py
@@ -1,10 +1,10 @@
 import src.main as main
 
 if __name__ == '__main__':
-    model = main.Main(practie_name="vgg16-test",
+    model = main.Main(practie_name="vgg16-80-128-100-hsv",
                       model_name="vgg16",
                       image_size_x=80,
                       image_size_y=80)
     model.images_create()
-    model.train(batch_size=32, epoch_size=100)
+    model.train(batch_size=128, epoch_size=100)
     model.test()
