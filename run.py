2af25cedb317ae87b1f082a688c0a7883fa6803d
Gen Takagi <hamham37337@gmail.com>
2021-03-06 20:16:12 +0900

    new

--- run.py
+++ run.py
@@ -1,7 +1,7 @@
 import src.main as main
 
 if __name__ == '__main__':
-    model = main.Main(practie_name="resnet50-fine_tuning-3",
+    model = main.Main(practie_name="resnet50-fine_tuning-4",
                       model_name="resnet50",
                       img_height=235,
                       img_width=80,
@@ -9,7 +9,7 @@ if __name__ == '__main__':
                       fine_tuning=True)
 
     # model.images_create()
-    model.train(batch_size=512, epoch_size=300)
+    model.train(batch_size=128, epoch_size=300)
     # model.train_nfold(n_splits=5, batch_size=128, epoch_size=100)
     # weights = ["xception-80-128-100-hsv"]
     # model.test(weights)
